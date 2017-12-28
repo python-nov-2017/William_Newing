4a660f129ce2a491cdca7fd6ca016bb20f79fab1
williamnewing <william.dean.newing@gmail.com>
2017-12-28 15:42:18 -0800

    UsersHW

---
+++ William_Newing/Django/users/apps/user_login/views.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Go make shell queries!")
+    
